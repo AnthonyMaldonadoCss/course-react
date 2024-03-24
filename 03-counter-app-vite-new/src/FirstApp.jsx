b160bdabe2a3f0706a8ec872310b439452c7cddf
Anthony Maldonado <anthony@clay.cl>
2024-03-23 23:22:29 -0400

    primeros componentes

---
+++ 03-counter-app-vite-new/src/FirstApp.jsx
@@ -0,0 +1,8 @@
+export const FirstApp = () => {
+  return (
+    <> 
+      <h1> Anthony: Software Developer </h1> 
+      <p> El fragment es para devolver lo que se quiere renderizar en un elemento padre </p>
+    </>
+  )
+}
