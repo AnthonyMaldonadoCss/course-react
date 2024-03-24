af9bec6a6ac24295a45e6dd617a116fa1bc8ca4c
Anthony Maldonado <anthony@clay.cl>
2024-03-24 00:00:51 -0400

    loop en elementos, stylos inline

--- 03-counter-app-vite-new/src/FirstApp.jsx
+++ 03-counter-app-vite-new/src/FirstApp.jsx
@@ -1,8 +1,26 @@
 export const FirstApp = () => {
+  const nombre = 'Anthony Maldonado';
+  const date = new Date();
+
+  const numeros = [1,2,3,4,5,6,7,8,9,10];
+
+  const loop = () => {
+    const li = [];
+    for (let [i,numero] of numeros.entries()) {
+      li.push( <li key={i} > {numero} </li> )
+    }
+    return li
+  }
+
+
   return (
     <> 
-      <h1> Anthony: Software Developer </h1> 
+      <h1> { nombre }: Software Developer </h1> 
+      <p> { date.getUTCDate() } - { date.getUTCMonth() } - { date.getFullYear() } </p>
       <p> El fragment es para devolver lo que se quiere renderizar en un elemento padre </p>
+      <ul style={ { display: "flex", flexDirection: "row", justifyContent:"space-evenly", alignItems: "flex-start"} }>
+        <> { loop() } </>
+      </ul>
     </>
   )
 }
