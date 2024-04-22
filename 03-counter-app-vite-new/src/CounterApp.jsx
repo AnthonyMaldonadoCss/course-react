3103c6031ed7f89346bee3e714e7d2f0e5a922db
Anthony Maldonado <anthony@clay.cl>
2024-04-21 21:25:46 -0400

    Hooks - multiple math operations

--- 03-counter-app-vite-new/src/CounterApp.jsx
+++ 03-counter-app-vite-new/src/CounterApp.jsx
@@ -7,11 +7,26 @@ export const CounterApp = ({ value }) => {
   const [ counter, setCounter ] = useState( value );
 
   const handleClick = (event) => {
+    console.log(value);
+    const { action } = event.target.dataset;
     console.log( 'Hola mundo' )
     console.log( event );
+    console.log( event.target.dataset.action );
+
+    switch ( action ) {
+      case 'add':
+        setCounter( counter + 1 );
+        break;
+      case 'substract':
+        setCounter( counter - 1 );
+        break;
+      case 'reset':
+        setCounter( value );
+        break;
+    }
   
-    setCounter( counter + 1 );
-    //setCounter( (c) => c + 1 );
+    // setCounter( counter + 1 );
+    // setCounter( (c) => c + 1 );
   
   }
 
@@ -19,9 +34,9 @@ export const CounterApp = ({ value }) => {
     <>
       <h1> CounterApp </h1>
       <h2> { counter } </h2>
-      <button onClick={ handleClick }>
-        +1
-      </button>
+      <button onClick={ handleClick } data-action='add'> +1 </button> &nbsp;
+      <button onClick={ handleClick } data-action='substract'> -1 </button> &nbsp;
+      <button onClick={ handleClick } data-action='reset'> Reset </button> 
     </>
   )
 }
