8640a7f7e3217c9ce596959c0fa66c172cc12cc5
Anthony Maldonado <anthony@clay.cl>
2024-04-21 21:09:42 -0400

    Hooks - UseState

--- 03-counter-app-vite-new/src/CounterApp.jsx
+++ 03-counter-app-vite-new/src/CounterApp.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
+
+
 export const CounterApp = ({ value }) => {
+  const [ counter, setCounter ] = useState( value );
+
+  const handleClick = (event) => {
+    console.log( 'Hola mundo' )
+    console.log( event );
+  
+    setCounter( counter + 1 );
+    //setCounter( (c) => c + 1 );
+  
+  }
 
   return (
     <>
       <h1> CounterApp </h1>
-      <h2> { value } </h2>
+      <h2> { counter } </h2>
+      <button onClick={ handleClick }>
+        +1
+      </button>
     </>
   )
 }
