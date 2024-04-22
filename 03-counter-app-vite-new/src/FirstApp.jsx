f388c918eeac36c2c451e4326f46fc716a8d0501
Anthony Maldonado <anthony@clay.cl>
2024-04-21 20:09:58 -0400

    PropTypes and DefaultTypes

--- 03-counter-app-vite-new/src/FirstApp.jsx
+++ 03-counter-app-vite-new/src/FirstApp.jsx
@@ -1,26 +1,61 @@
-export const FirstApp = () => {
-  const nombre = 'Anthony Maldonado';
-  const date = new Date();
+import PropTypes from 'prop-types';
 
-  const numeros = [1,2,3,4,5,6,7,8,9,10];
 
-  const loop = () => {
-    const li = [];
-    for (let [i,numero] of numeros.entries()) {
-      li.push( <li key={i} > {numero} </li> )
-    }
-    return li
+const nombre = 'Anthony Maldonado';
+const date = new Date();
+
+const numeros = [1,2,3,4,5,6,7,8,9,10];
+
+const loop = () => {
+  const li = [];
+  for (let [i,numero] of numeros.entries()) {
+    li.push( <li key={i} > {numero} </li> )
   }
+  return li
+}
+/**ES MEJOR DECLARAR LAS FUNCIONES FUERA PORQUE AL MOMENTO DE RENDERIZAR 
+ * SE RE EVALURAN LAS FUNCIONES, CONSTANTES, ETC...
+*/
 
+export const FirstApp = ({title, subTitle}) => {
+
+  // console.log(props);
 
   return (
     <> 
-      <h1> { nombre }: Software Developer </h1> 
+      <h1 style={ { color: "red" } } > 
+        { nombre }: 
+        <span style={ { color: "blue", fontSize:"xx-large" } }> 
+          &nbsp; Software Developer 
+        </span>
+      </h1> 
+
+      <h2>
+        { title }
+      </h2>
+
+      <h4>
+        { subTitle } 
+      </h4>
+      
       <p> { date.getUTCDate() } - { date.getUTCMonth() } - { date.getFullYear() } </p>
+      
       <p> El fragment es para devolver lo que se quiere renderizar en un elemento padre </p>
+      
       <ul style={ { display: "flex", flexDirection: "row", justifyContent:"space-evenly", alignItems: "flex-start"} }>
         <> { loop() } </>
       </ul>
+    
     </>
   )
+}
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.number.isRequired
+}
+
+FirstApp.defaultProps = {
+  title: 'No hay titulo',
+  subTitle: 'qq1111'
 }
