52f49f23f516017facf824b93e82ab2811fad739
Anthony Maldonado <anthony@clay.cl>
2024-04-27 19:34:39 -0400

    Bases de pruebas

---
+++ 03-counter-app-vite-new/src/base-pruebas/11-async-await.js
@@ -0,0 +1,20 @@
+export const getImagen = async() => {
+
+    try {
+
+        const apiKey = 't7ZFv2PsjHt6Ou1QPCmz04iHUK4jiRdq';
+        const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+        const { data } = await resp.json(); 
+
+        const { url } = data.images.original;
+        return url
+
+    } catch (error) {
+        // manejo del error
+        console.error(error)
+        return 'No se encontró la imagen';
+    }
+}
+
+
+
