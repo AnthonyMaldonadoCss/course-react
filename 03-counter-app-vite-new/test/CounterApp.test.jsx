49cd8f8dcf1a099cdd3b094d7e775ccce82f59c5
Anthony Maldonado <anthony@clay.cl>
2024-04-28 23:23:31 -0400

    Pruebas con Evento click
    Selectores de elementos HTML

--- 03-counter-app-vite-new/test/CounterApp.test.jsx
+++ 03-counter-app-vite-new/test/CounterApp.test.jsx
@@ -1,5 +1,5 @@
 import { CounterApp } from "../src/CounterApp";
-import { render, screen } from "@testing-library/react"
+import { render, screen, fireEvent } from "@testing-library/react"
 
 describe('Haciendo pruebas en el <CounterApp />', () => {
   
@@ -18,10 +18,39 @@ describe('Haciendo pruebas en el <CounterApp />', () => {
   test('Deben existir los botones de incrementar, decrementar y reset', () => {
     const initialValue = 100
     render( <CounterApp value={ initialValue } /> );
-    expect( screen.getByRole('button', { name: 'Reset' } ) ).toBeTruthy();
+    // expect( screen.getByRole('button', { name: 'Reset' } ) ).toBeTruthy();
     expect( screen.getByRole('button', { name: '+1' } ) ).toBeTruthy();
     expect( screen.getByRole('button', { name: '-1' } ) ).toBeTruthy();
+    
+    expect( screen.getByRole('button', { name: 'reset' } ) ).toBeTruthy();
+
+    
   })
+
+  test('Debe de incrementar al hacer click en el +1', () => {
+    const initialValue = 100
+    render( <CounterApp value={ initialValue } /> );
+    fireEvent.click( screen.getByRole('button', { name: '+1' } ) );
+    expect( screen.getByText( 101 ) ).toBeTruthy();
+    expect( screen.getByRole('heading', { level: 2 } ).innerHTML ).toBe( ' 101 ' );
+  })
+
+  test('Debe de decrementar al hacer click en el -1', () => {
+    const initialValue = 100
+    render( <CounterApp value={ initialValue } /> );
+    fireEvent.click( screen.getByRole('button', { name: '-1' } ) );
+    expect( screen.getByText( 99 ) ).toBeTruthy();
+    expect( screen.getByRole('heading', { level: 2 } ).innerHTML ).toBe( ' 99 ' );
+  })
+
+  test('Debe de resetear el valor al valor inicial del componente', () => {
+    const initialValue = 100
+    render( <CounterApp value={ initialValue } /> );
+    fireEvent.click( screen.getByRole('button', { name: 'reset' } ) );
+    expect( screen.getByText( initialValue ) ).toBeTruthy();
+    expect( screen.getByRole('heading', { level: 2 } ).innerHTML ).toBe( ' 100 ' );
+  })
+  
   
 
 })
