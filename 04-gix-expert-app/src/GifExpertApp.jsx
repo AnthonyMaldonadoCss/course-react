dccea45555f64a30b6c86fd6dfb72809dbd5e51c
Anthony Maldonado <anthony@clay.cl>
2024-04-30 21:04:12 -0400

    Comunicandonos con el componente Hijo

--- 04-gix-expert-app/src/GifExpertApp.jsx
+++ 04-gix-expert-app/src/GifExpertApp.jsx
@@ -1,9 +1,37 @@
-
+import { useState } from 'react'
+import AddCategory from './components/AddCategory';
 
 const GifExpertApp = () => {
+  
+  const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+  console.log(categories);
+
+  const onAddCategory = (NewCategory) => {
+    console.log(NewCategory);
+    setCategories([ ...categories, NewCategory ]);
+  }
+
   return (
     <>
-      <h1>GifExpertApp</h1> 
+      {/* titulo */}
+      <h1>GifExpertApp</h1>
+      
+      {/* input */}
+      <AddCategory 
+        // setCategories={ setCategories } 
+        onNewCategory={ onAddCategory }
+      />
+      {/* <button onClick={ onAddCategory } >Agregar categoría</button> */}
+      
+      {/* Listado de categorias */}
+      <ol>
+        {
+          categories.map((category, index) => (
+            <li key={ index }> { category.toUpperCase() } </li>
+          ))
+        }
+      </ol>
     </>
   )
 }
