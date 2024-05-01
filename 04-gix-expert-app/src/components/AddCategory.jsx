8da2d007341d9c6d1295159666b537d7778040db
Anthony Maldonado <anthony@clay.cl>
2024-04-30 21:03:43 -0400

    Componente de agregar categorias

---
+++ 04-gix-expert-app/src/components/AddCategory.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import PropTypes from 'prop-types';
+
+export default function AddCategory( { onNewCategory } ) {
+  
+  const [inputValue, setInputValue] = useState('');
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value)
+    
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    
+    if (inputValue.trim().length <= 1) return;
+    
+    onNewCategory( inputValue.trim() );
+    setInputValue('');
+  }
+
+  return (
+    <form onSubmit={ onSubmit }>
+      <input 
+        type="text"
+        placeholder="Buscar Gifs"
+        value={ inputValue }
+        onChange={ onInputChange }
+      />
+    </form>
+    
+  )
+}
+
+AddCategory.propTypes = {
+  onNewCategory: PropTypes.func
+}
